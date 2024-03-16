3509bb71f62e162663f5d276f7aaaba2808e4c06
Miguel Augusto da Silva <040709augustomiguel@gmail.com>
2024-03-15 21:46:07 -0300

    BASE CONFIG - antes da reunião com os pastores e arrumado codigo

--- static/js/animação.js
+++
@@ -1,8 +0,0 @@
-ScrollReveal({
-    reset: true,
-    distance: '60px',
-    duration: 1000,
-});
-
-ScrollReveal().reveal('.activate', {origin: 'left'})
-ScrollReveal().reveal('.activate_rigth', {origin: 'rigth'})
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,30 @@
+const Menu_mobile = document.getElementById('menu_nav_mobile')
+
+Menu_mobile.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    Menu_mobile.classList.toggle('menu_mobile_ativo');
+}
+
+var slide_ = document.querySelector('manual_btn')
+var conteudo = 1
+
+document.getElementById('slide_1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    conteudo++
+
+    if(conteudo > 3){
+        conteudo = 1
+    }
+
+    document.getElementById('slide_'+conteudo).checked = true
+}
+
 const dias = document.getElementById("dias")
 const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
@@ -42,3 +69,11 @@ function fim_time(time){
 tempo();
 
 setInterval(tempo, 1000);
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 1000,
+});
+
+ScrollReveal().reveal('.activate', {origin: 'left'});
